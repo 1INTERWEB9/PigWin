83f263de6965ed83a21272d09267c7630ccd051e
INTERWEB <bencito10@gmail.com>
2023-10-30 07:55:06 -0500

    [UPDATE] index.js (rename of main.js)

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const { connection } = require("./database");
 
 //Initialization express
 const app = express();
@@ -8,13 +11,16 @@ app.set("port", 3000);
 //Initialization middleware
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("dev"));
 
 app.listen(app.get("port"), () => {
+  connection.sync({ force: true });
   console.log(`Server online: http://localhost:${app.get("port")}`);
 });
 
 //Import routes
-app.use("/query", require("./routes/queriesRoutes"));
+// app.use("/query", require("./routes/queriesRoutes"));
 
 //If a route doesn't exist
 app.use((req, res) => {
